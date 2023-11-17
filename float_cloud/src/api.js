6e187ab71b804a8d4df16d71f8ef9d86c86ff13a
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2023-11-18 04:48:50 +0900

    feat: api 연결

--- float_cloud/src/api.js
+++ float_cloud/src/api.js
@@ -1,2 +1,47 @@
+/*global chrome*/
 import axios from 'axios';
 const baseURL = process.env.REACT_APP_URL;
+
+const instanceUtil = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type" : "application/json",
+  },
+});
+
+instanceUtil.interceptors.request.use(
+  (config) => {
+    chrome.storage.local.get('token', (result) => {
+      if (result.token) {
+        config.headers.Authorization = result.token;
+      } else {
+        window.open("https://www.ddngoorm.xyz/login", "_blank", "noopener,noreferrer");
+      } return config;
+    });
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export const getTeams = async () => {
+  try {
+    const response = await instanceUtil.get(`/teams`);
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+export const postCloud = async (data) => {
+  try {
+    const response = await instanceUtil.post(`/guest/clouds`, data);
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
